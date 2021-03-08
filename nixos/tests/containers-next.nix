4ec5c6a9f05a11e8901271ad2ddc50f4e1f4a533
Maximilian Bosch <maximilian@mbosch.me>
2021-03-08 22:52:25 +0100

    WIP - demo for MACVLANs
    After trying to write an abstraction for it I decided against it and
    leave this up to the administrator. This new module allows an (IMHO)
    better integration with systemd, so using such features directly isn't a
    big deal I guess.
    
    For more context, I left a comment in the test.

--- nixos/tests/containers-next.nix
+++ nixos/tests/containers-next.nix
@@ -10,7 +10,7 @@ import ./make-test-python.nix ({ pkgs, lib, ... }: {
   # Just an arbitrary `client'-machine to test the public endpoints
   # of containers hosted on a different server.
   nodes.client = { pkgs, ... }: {
-    virtualisation.vlans = [ 1 ];
+    virtualisation.vlans = [ 1 2 ];
     boot.consoleLogLevel = 7;
     environment.systemPackages = [ pkgs.tcpdump pkgs.tmux ];
     systemd.network.networks."10-eth1" = {
@@ -24,10 +24,82 @@ import ./make-test-python.nix ({ pkgs, lib, ... }: {
         { routeConfig.Destination = "fd24::1/64"; }
       ];
     };
+    systemd.network.networks."10-eth2" = {
+      matchConfig.Name = "eth2";
+      networkConfig = {
+        DHCP = "yes";
+      };
+      address = [ "192.168.2.1/24" ];
+      linkConfig.RequiredForOnline = "no";
+    };
     networking = {
       useNetworkd = true;
       useDHCP = false;
       interfaces.eth0.useDHCP = true;
+    };
+  };
+
+  # Test environment for MACVLAN functionality.
+  #
+  # Just as it was the case for the existing `nixos-container` implementation, I originally
+  # planned an abstraction here as well, but decided against it for the following
+  # reasons:
+  # * systemd-networkd already provides well-designed abstractions for network configurations
+  #   and a certain degree of declarativity.
+  #
+  # * in case of networkd we'd have to (1) create a host-interface for a MACVLAN (of course)
+  #   and declare it as macvlan interface in the config of the *physical interface itself*.
+  #   This means that we'd need a way to declare this in NixOS which turns out to be non-trivial
+  #   since networkd only uses the first `.network` file (in lexical order) it can find
+  #   so there's a risk that we'd invalidate other configurations with this.
+  #
+  # So to summarize, the abstractions I tried were leaky and IMHO useless. But now that we can
+  # use systemd-nspawn itself for containers (and consider NixOS just a thin abstraction layer),
+  # this isn't a big deal IMHO.
+  nodes.macvlan = { pkgs, lib, ... }: {
+    virtualisation.vlans = [ 2 ];
+    boot.consoleLogLevel = 7;
+    environment.systemPackages = [ pkgs.tcpdump pkgs.tmux ];
+    systemd.network.networks."40-eth1" = {
+      matchConfig.Name = "eth1";
+      networkConfig.DHCP = lib.mkForce "yes";
+      networkConfig.MACVLAN = "mv-eth1";
+      linkConfig.RequiredForOnline = "no";
+      address = lib.mkForce [];
+      addresses = lib.mkForce [];
+    };
+    systemd.network.networks."20-mv-eth1" = {
+      matchConfig.Name = "mv-eth1";
+      networkConfig.IPForward = "yes";
+      address = lib.mkForce [
+        "192.168.2.2/24"
+      ];
+    };
+    systemd.network.netdevs."20-mv-eth1" = {
+      netdevConfig = {
+        Name = "mv-eth1";
+        Kind = "macvlan";
+      };
+      extraConfig = ''
+        [MACVLAN]
+        Mode=bridge
+      '';
+    };
+    systemd.nspawn.vlandemo.networkConfig.MACVLAN = "eth1";
+    networking = {
+      useNetworkd = true;
+      useDHCP = false;
+      interfaces.eth0.useDHCP = true;
+    };
+    nixos.containers = {
+      instances.vlandemo.config = {
+        systemd.network = {
+          networks."10-mv-eth1" = {
+            matchConfig.Name = "mv-eth1";
+            address = [ "192.168.2.5/24" ];
+          };
+        };
+      };
     };
   };
 
@@ -126,20 +198,31 @@ import ./make-test-python.nix ({ pkgs, lib, ... }: {
     client.wait_for_unit("network-online.target")
     client.wait_for_unit("multi-user.target")
 
+    client.wait_for_unit("systemd-networkd-wait-online.service")
+    macvlan.wait_for_unit("multi-user.target")
+
     server.wait_for_unit("systemd-nspawn@container0")
     server.wait_for_unit("systemd-networkd-wait-online.service")
-    client.wait_for_unit("systemd-networkd-wait-online.service")
 
-    server.execute("ping fd24::1 -c3 >&2")
-    server.execute("ping fd24::2 -c3 >&2 || true")
-    client.execute("ping fd24::1 -c3 >&2 || true")
-    server.wait_until_succeeds("ping -4 container0 -c3 >&2")
-    server.wait_until_succeeds("ping -6 container0 -c3 >&2")
+    with subtest("Static networking"):
+        server.execute("ping fd24::1 -c3 >&2")
+        server.execute("ping fd24::2 -c3 >&2 || true")
+        client.execute("ping fd24::1 -c3 >&2 || true")
+        server.wait_until_succeeds("ping -4 container0 -c3 >&2")
+        server.wait_until_succeeds("ping -6 container0 -c3 >&2")
 
-    server.wait_until_succeeds("curl -sSf 'http://[fd24::2]' | grep -q 'Welcome to nginx'")
+        server.wait_until_succeeds(
+            "curl -sSf 'http://[fd24::2]' | grep -q 'Welcome to nginx'"
+        )
 
-    client.wait_until_succeeds("ping fd24::2 -c3 >&2")
-    client.succeed("curl -sSf 'http://[fd24::2]' | grep -q 'Welcome to nginx'")
+        client.wait_until_succeeds("ping fd24::2 -c3 >&2")
+        client.succeed("curl -sSf 'http://[fd24::2]' | grep -q 'Welcome to nginx'")
+
+    with subtest("MACVLANs"):
+        macvlan.wait_until_succeeds("ping 192.168.2.2 -c3 >&2")
+        macvlan.wait_until_succeeds("ping 192.168.2.5 -c3 >&2")
+        client.wait_until_succeeds("ping 192.168.2.2 -c3 >&2")
+        client.wait_until_succeeds("ping 192.168.2.5 -c3 >&2")
 
     server.succeed("machinectl poweroff container0")
     server.succeed("machinectl poweroff container1")
@@ -147,7 +230,10 @@ import ./make-test-python.nix ({ pkgs, lib, ... }: {
     server.wait_until_unit_stops("systemd-nspawn@container0")
     server.wait_until_unit_stops("systemd-nspawn@container1")
 
+    macvlan.succeed("machinectl poweroff vlandemo")
+
     client.shutdown()
     server.shutdown()
+    macvlan.shutdown()
   '';
 })
