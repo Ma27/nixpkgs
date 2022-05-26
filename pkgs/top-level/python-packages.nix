f04b91306c4dc5d58a3a34a5e11f276f44f012f1
Robert Schütz <nix@dotlambda.de>
2022-05-26 18:26:27 +0000

    python3Packages.docformatter: init at 1.4
    (cherry picked from commit 746256345f12f2278632217fa9ec13d6de8dd9b4)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2475,6 +2475,8 @@ in {
 
   doc8 = callPackage ../development/python-modules/doc8 { };
 
+  docformatter = callPackage ../development/python-modules/docformatter { };
+
   docker = callPackage ../development/python-modules/docker { };
 
   dockerfile-parse = callPackage ../development/python-modules/dockerfile-parse { };
