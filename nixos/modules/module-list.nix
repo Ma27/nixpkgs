c95f805d386690ff0fb1a9514dac877b5aed2237
Maximilian Bosch <maximilian@mbosch.me>
2022-06-12 18:58:57 +0200

    Backport changes of networkd-containers to test on my infrastructure
    Applied the diff from https://github.com/NixOS/nixpkgs/pull/140669/
    at revision cd533c38caa5e71be6ed6865872e2b31100e19e1.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1421,6 +1421,9 @@
   ./virtualisation/container-config.nix
   ./virtualisation/containerd.nix
   ./virtualisation/containers.nix
+  ./virtualisation/containers-next
+  ./virtualisation/nixos-containers.nix
+  ./virtualisation/oci-containers.nix
   ./virtualisation/cri-o.nix
   ./virtualisation/docker-rootless.nix
   ./virtualisation/docker.nix
