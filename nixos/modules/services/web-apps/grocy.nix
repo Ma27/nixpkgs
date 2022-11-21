422e03d7d82479a15204e1f33b4034e63ad13fa1
Maximilian Bosch <maximilian@mbosch.me>
2022-11-21 23:37:20 +0100

    grocy: use php8 and fix w/ ldap changes

--- nixos/modules/services/web-apps/grocy.nix
+++ nixos/modules/services/web-apps/grocy.nix
@@ -193,9 +193,9 @@ in {
       user = "grocy";
       group = "nginx";
 
-      # PHP 7.4 is the only version which is supported/tested by upstream:
-      # https://github.com/grocy/grocy/blob/v3.0.0/README.md#how-to-install
-      phpPackage = pkgs.php74;
+      # PHP 8.0 is the only version which is supported/tested by upstream:
+      # https://github.com/grocy/grocy/blob/v3.3.2/README.md#how-to-install
+      phpPackage = pkgs.php80;
 
       inherit (cfg.phpfpm) settings;
 
