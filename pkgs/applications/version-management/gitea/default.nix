a0d5f3c1d582758b1523d4a94b1a7f7c32475f38
techknowlogick <techknowlogick@gitea.com>
2023-11-14 16:12:33 -0500

    gitea: 1.20.5 -> 1.21.1
    (cherry picked from commit c70d0c8ce362a0671a2bbbcd97c719589b6dcb14)

--- pkgs/applications/version-management/gitea/default.nix
+++ pkgs/applications/version-management/gitea/default.nix
@@ -21,12 +21,12 @@
 
 buildGoModule rec {
   pname = "gitea";
-  version = "1.20.6";
+  version = "1.21.1";
 
   # not fetching directly from the git repo, because that lacks several vendor files for the web UI
   src = fetchurl {
     url = "https://dl.gitea.com/gitea/${version}/gitea-src-${version}.tar.gz";
-    hash = "sha256-KB7DRWAdefgLfXik+DYA6sI1XMBiSrSWoI+1YlxWalQ=";
+    hash = "sha256-5WEHUMQsQNgrglS+xJ4IWHUl0a6RLLPyx0l+ECJ4R9g=";
   };
 
   vendorHash = null;
