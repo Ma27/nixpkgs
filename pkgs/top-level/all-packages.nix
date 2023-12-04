a9bc1b361653a89467aa80b306c044f050975e0c
Maximilian Bosch <maximilian@mbosch.me>
2023-12-04 15:55:23 +0100

    grafana: build w/ go 1.21

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25438,7 +25438,7 @@ with pkgs;
 
   gofish = callPackage ../servers/gopher/gofish { };
 
-  grafana = callPackage ../servers/monitoring/grafana { };
+  grafana = callPackage ../servers/monitoring/grafana { buildGoModule = buildGo121Module; };
   grafanaPlugins = callPackages ../servers/monitoring/grafana/plugins { };
 
   grafana-agent = callPackage ../servers/monitoring/grafana-agent { };
