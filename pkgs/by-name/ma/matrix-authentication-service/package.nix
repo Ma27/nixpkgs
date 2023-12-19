7cb6ccebc4f0db0275a18ecaa8a7966e5272e8c5
teutat3s <10206665+teutat3s@users.noreply.github.com>
2023-12-19 01:53:59 +0100

    nixos/matrix-authentication-service: init
    (cherry picked from commit c3fcdfde109b6cc9ab4b82e73d9bdf82e9ff2324)

--- pkgs/by-name/ma/matrix-authentication-service/package.nix
+++ pkgs/by-name/ma/matrix-authentication-service/package.nix
@@ -14,6 +14,7 @@
   cctools,
   nix-update-script,
   versionCheckHook,
+  nixosTests,
 }:
 
 rustPackages_1_88.rustPlatform.buildRustPackage (finalAttrs: {
@@ -90,12 +91,16 @@ rustPackages_1_88.rustPlatform.buildRustPackage (finalAttrs: {
   versionCheckProgram = "${placeholder "out"}/bin/${finalAttrs.meta.mainProgram}";
   versionCheckProgramArg = "--version";
   doInstallCheck = true;
-  passthru.updateScript = nix-update-script {
-    extraArgs = [
-      # avoid unstable pre‐releases
-      "--version-regex"
-      "^v([0-9.]+)$"
-    ];
+
+  passthru = {
+    updateScript = nix-update-script {
+      extraArgs = [
+        # avoid unstable pre‐releases
+        "--version-regex"
+        "^v([0-9.]+)$"
+      ];
+    };
+    tests = { inherit (nixosTests) matrix-authentication-service; };
   };
 
   meta = {
