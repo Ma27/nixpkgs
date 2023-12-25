ba4c673f9989a3c47cd837f847780504c6d5a75a
Maximilian Bosch <maximilian@mbosch.me>
2023-12-25 14:07:45 +0100

    neomutt: 20231103 -> 20231221
    ChangeLog: https://github.com/neomutt/neomutt/releases/tag/20231221
    
    * Dropped `fix-open-very-large-mailbox.patch`: doesn't apply anymore,
      but I cannot reproduce the behavior anymore (i.e. crashes when opening
      mailboxes with >40k mails).
    
      Should be tested by somebody else just to be sure though.
    
    * `fix-attr-color-copy.patch` is part of this release.
    
    (cherry picked from commit 11f58647d68261dfa90de2da61a7858abac7a9cc)

--- pkgs/applications/networking/mailreaders/neomutt/default.nix
+++ pkgs/applications/networking/mailreaders/neomutt/default.nix
@@ -6,20 +6,15 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "20231023";
+  version = "20231221";
   pname = "neomutt";
 
   src = fetchFromGitHub {
     owner  = "neomutt";
     repo   = "neomutt";
     rev    = version;
-    sha256 = "sha256-62WnL3E7acWBo7bgaa0DTyHMXezUQOEOofYI1/e7M+s=";
+    sha256 = "sha256-IXly2N/DD2+XBXVIXJw1sE/0eJwbUaONDNRMi7n1T44=";
   };
-
-  patches = [
-    # https://github.com/neomutt/neomutt/issues/3773#issuecomment-1493295144
-    ./fix-open-very-large-mailbox.patch
-  ];
 
   buildInputs = [
     cyrus_sasl gss gpgme libkrb5 libidn2 ncurses
