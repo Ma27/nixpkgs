d2987e0379c02072fd88e4daf22864955e001c37
Maximilian Bosch <maximilian@mbosch.me>
2024-04-10 10:04:43 +0200

    nixos/udev: compress firmware with zstd if possible
    Closes #267442
    
    (cherry picked from commit 2e6254145f70cc90ff4e349043cfde00fee42c01)

--- nixos/modules/services/hardware/udev.nix
+++ nixos/modules/services/hardware/udev.nix
@@ -166,10 +166,17 @@ let
       mv etc/udev/hwdb.bin $out
     '';
 
-  compressFirmware = firmware: if (config.boot.kernelPackages.kernelAtLeast "5.3" && (firmware.compressFirmware or true)) then
-    pkgs.compressFirmwareXz firmware
-  else
-    id firmware;
+  compressFirmware = firmware:
+    let
+      inherit (config.boot.kernelPackages) kernelAtLeast;
+      compress =
+        if ! (firmware.compressFirmware or true) then
+          id
+        else
+          if kernelAtLeast "5.19" then pkgs.compressFirmwareZstd
+          else if kernelAtLeast "5.3" then pkgs.compressFirmwareXz
+          else id;
+    in compress firmware;
 
   # Udev has a 512-character limit for ENV{PATH}, so create a symlink
   # tree to work around this.
--- pkgs/build-support/kernel/compress-firmware.nix
+++ pkgs/build-support/kernel/compress-firmware.nix
@@ -1,24 +1,38 @@
-{ runCommand, lib }:
+{ runCommand, lib, type ? "zstd", zstd }:
 
 firmware:
 
 let
+  compressor = {
+    xz = {
+      ext = "xz";
+      nativeBuildInputs = [ ];
+      cmd = file: target: ''xz -9c -T1 -C crc32 --lzma2=dict=2MiB "${file}" > "${target}"'';
+    };
+    zstd = {
+      ext = "zst";
+      nativeBuildInputs = [ zstd ];
+      cmd = file: target: ''zstd -T1 -6 --check -f "${file}" -o "${target}"'';
+    };
+  }.${type} or (throw "Unsupported compressor type for firmware.");
+
   args = {
     allowedRequisites = [];
+    inherit (compressor) nativeBuildInputs;
   } // lib.optionalAttrs (firmware ? meta) { inherit (firmware) meta; };
 in
 
-runCommand "${firmware.name}-xz" args ''
+runCommand "${firmware.name}-${type}" args ''
   mkdir -p $out/lib
   (cd ${firmware} && find lib/firmware -type d -print0) |
       (cd $out && xargs -0 mkdir -v --)
   (cd ${firmware} && find lib/firmware -type f -print0) |
       (cd $out && xargs -0rtP "$NIX_BUILD_CORES" -n1 \
-          sh -c 'xz -9c -T1 -C crc32 --lzma2=dict=2MiB "${firmware}/$1" > "$1.xz"' --)
+          sh -c '${compressor.cmd "${firmware}/$1" "$1.${compressor.ext}"}' --)
   (cd ${firmware} && find lib/firmware -type l) | while read link; do
       target="$(readlink "${firmware}/$link")"
       if [ -f "${firmware}/$link" ]; then
-        ln -vs -- "''${target/^${firmware}/$out}.xz" "$out/$link.xz"
+        ln -vs -- "''${target/^${firmware}/$out}.${compressor.ext}" "$out/$link.${compressor.ext}"
       else
         ln -vs -- "''${target/^${firmware}/$out}" "$out/$link"
       fi
