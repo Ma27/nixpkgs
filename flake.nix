a843b53b15c24ce33703e7a8da80213f81128427
Maximilian Bosch <maximilian@mbosch.me>
2024-05-18 15:18:17 +0200

    flake: allow widevine-cdm as unfree software

--- flake.nix
+++ flake.nix
@@ -222,6 +222,7 @@
                 "chrome-widevine-cdm"
                 "chromium"
                 "chromium-binary-plugin-widevine"
+                "widevine-cdm"
                 "chromium-unwrapped"
                 "spotify"
                 "spotify-unwrapped"
