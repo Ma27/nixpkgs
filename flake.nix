4dadbf2ba9bf21f48b72d9a7eb6bfa60e8c48bc4
Maximilian Bosch <maximilian@mbosch.me>
2024-09-10 13:42:24 +0200

    flake: whitelist olm

--- flake.nix
+++ flake.nix
@@ -107,6 +107,9 @@
                 "1password-cli"
               ];
             chromium.enableWideVine = true;
+            permittedInsecurePackages = [
+              "olm-3.2.16"
+            ];
           };
         }).extend (final: prev: {
           lib = prev.lib.extend libVersionInfoOverlay;
