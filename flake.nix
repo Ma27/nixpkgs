6939fd24fb90f151c6e4bc3e3337a44d6d914fcd
Maximilian Bosch <maximilian@mbosch.me>
2024-09-10 13:42:24 +0200

    flake: whitelist olm

--- flake.nix
+++ flake.nix
@@ -243,6 +243,9 @@
                 "1password-cli"
               ];
             chromium.enableWideVine = true;
+            permittedInsecurePackages = [
+              "olm-3.2.16"
+            ];
           };
           overlays = import ./pkgs/top-level/impure-overlays.nix ++ [
             (final: prev: {
