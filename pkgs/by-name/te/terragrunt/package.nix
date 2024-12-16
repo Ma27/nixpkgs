12a904fc3c00989126f9d9ffd14ca3aa9c74e9c8
Maximilian Bosch <maximilian@mbosch.me>
2024-12-16 17:00:37 +0100

    Revert terragrunt update (0.69.10 -> 0.68.7)
    This reverts commit 1b30a61aa9090d38d969ecb3fe40123503464987.
    This reverts commit b1049b5d10a99bcffb13aa7da57bb7f08a4b4bc1.
    
    Patching in the age plugin support for sops is too annoying. Let's make
    the package fixed and call it a day (for now, I heard mic92 is about to
    fork the stuff anyways).

--- pkgs/by-name/te/terragrunt/package.nix
+++ pkgs/by-name/te/terragrunt/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "terragrunt";
-  version = "0.69.10";
+  version = "0.68.7";
 
   src = fetchFromGitHub {
     owner = "gruntwork-io";
     repo = pname;
-    tag = "v${version}";
-    hash = "sha256-V/ETnIcSr47w/MhluZ4BWDPz4S3ka5H1p2OiOAMPAiY=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-+66+qntwEILB0wbO/J5S6ED5W8JoE0C1KSGmGwIYGM0=";
   };
 
   nativeBuildInputs = [ go-mockery ];
@@ -22,7 +22,7 @@ buildGoModule rec {
     make generate-mocks
   '';
 
-  vendorHash = "sha256-cxs2BekBUqZFwc+MkabKSFQYAxmEdxe0eWwx60c6ofQ=";
+  vendorHash = "sha256-MLIj+/FUw7C4sOWby9nmcQFAvjVNvnf6bKs3pmlhzRQ=";
 
   doCheck = false;
 
