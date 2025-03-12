89aa54aa26687c25b978c1f51d6ddb7cfa86cd87
Maximilian Bosch <maximilian@mbosch.me>
2025-03-12 12:08:34 +0100

    grafana-image-renderer: 3.12.1 -> 3.12.3
    ChangeLog: https://github.com/grafana/grafana-image-renderer/blob/v3.12.3/CHANGELOG.md#3123-2025-03-12
    (cherry picked from commit d0ea799ea0570577bf1c8ccf4c758151f5cd81a6)

--- pkgs/by-name/gr/grafana-image-renderer/package.nix
+++ pkgs/by-name/gr/grafana-image-renderer/package.nix
@@ -1,10 +1,11 @@
 {
   lib,
-  mkYarnPackage,
   fetchFromGitHub,
   fetchYarnDeps,
-  nodejs,
+  nodejs_22,
+  yarn2nix-moretea,
   runtimeShell,
+  yarn,
 }:
 
 # Notes for the upgrade:
@@ -12,64 +13,72 @@
 # * Replace new `package.json` here.
 # * Update `version`+`hash` and rebuild.
 
-mkYarnPackage rec {
-  pname = "grafana-image-renderer";
-  version = "3.12.1";
+let
+  yarn' = yarn.override { nodejs = nodejs_22; };
+in
+((yarn2nix-moretea.override {
+  yarn = yarn';
+  nodejs = nodejs_22;
+}).mkYarnPackage
+)
+  rec {
+    pname = "grafana-image-renderer";
+    version = "3.12.3";
 
-  src = fetchFromGitHub {
-    owner = "grafana";
-    repo = "grafana-image-renderer";
-    rev = "v${version}";
-    hash = "sha256-j01C5h8RKZi/jcJyzXqgw0sAiBdVphi1kLxgqygVhkg=";
-  };
+    src = fetchFromGitHub {
+      owner = "grafana";
+      repo = "grafana-image-renderer";
+      rev = "v${version}";
+      hash = "sha256-twB2V5iecLEMTrp+prFmDoJvcGLVQlLJ+DTSl/9V8S8=";
+    };
 
-  offlineCache = fetchYarnDeps {
-    yarnLock = src + "/yarn.lock";
-    hash = "sha256-eYn69tlwCu3ohSCFdifMifvLgHgogv9aq6n8N363Hbw=";
-  };
+    offlineCache = fetchYarnDeps {
+      yarnLock = src + "/yarn.lock";
+      hash = "sha256-TcsWApSR48OUIcQGko3XIaFCp22vFJbHcxrdFxUyRZU=";
+    };
 
-  packageJSON = ./package.json;
+    packageJSON = ./package.json;
 
-  buildPhase = ''
-    runHook preBuild
+    buildPhase = ''
+      runHook preBuild
 
-    pushd deps/renderer
-    yarn run build
-    popd
+      pushd deps/renderer
+      yarn run build
+      popd
 
-    runHook postBuild
-  '';
+      runHook postBuild
+    '';
 
-  dontInstall = true;
+    dontInstall = true;
 
-  distPhase = ''
-    runHook preDist
+    distPhase = ''
+      runHook preDist
 
-    shopt -s extglob
+      shopt -s extglob
 
-    pushd deps/renderer
-    install_path="$out/libexec/grafana-image-renderer"
-    mkdir -p $install_path
-    cp -R ../../node_modules $install_path
-    cp -R ./!(node_modules) $install_path
-    popd
+      pushd deps/renderer
+      install_path="$out/libexec/grafana-image-renderer"
+      mkdir -p $install_path
+      cp -R ../../node_modules $install_path
+      cp -R ./!(node_modules) $install_path
+      popd
 
-    mkdir -p $out/bin
-    cat >$out/bin/grafana-image-renderer <<EOF
-    #! ${runtimeShell}
-    ${nodejs}/bin/node $install_path/build/app.js \$@
-    EOF
-    chmod +x $out/bin/grafana-image-renderer
+      mkdir -p $out/bin
+      cat >$out/bin/grafana-image-renderer <<EOF
+      #! ${runtimeShell}
+      ${nodejs_22}/bin/node $install_path/build/app.js \$@
+      EOF
+      chmod +x $out/bin/grafana-image-renderer
 
-    runHook postDist
-  '';
+      runHook postDist
+    '';
 
-  meta = with lib; {
-    homepage = "https://github.com/grafana/grafana-image-renderer";
-    description = "Grafana backend plugin that handles rendering of panels & dashboards to PNGs using headless browser (Chromium/Chrome)";
-    mainProgram = "grafana-image-renderer";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ ma27 ];
-    platforms = platforms.all;
-  };
-}
+    meta = with lib; {
+      homepage = "https://github.com/grafana/grafana-image-renderer";
+      description = "Grafana backend plugin that handles rendering of panels & dashboards to PNGs using headless browser (Chromium/Chrome)";
+      mainProgram = "grafana-image-renderer";
+      license = licenses.asl20;
+      maintainers = with maintainers; [ ma27 ];
+      platforms = platforms.all;
+    };
+  }
