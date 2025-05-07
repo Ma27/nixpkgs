9ef155ea28fba3457bcb86ce66cb6458b4924fbc
Maximilian Bosch <maximilian@mbosch.me>
2025-05-07 14:23:20 +0200

    grafana-image-renderer: 3.12.3 -> 3.12.5
    ChangeLogs:
    * https://github.com/grafana/grafana-image-renderer/releases/tag/v3.12.4
    * https://github.com/grafana/grafana-image-renderer/releases/tag/v3.12.5
    
    I decided against downgrading Node to v20, apparently, their downgrade
    is due to a matter of Windows packaging[1].
    
    [1] https://github.com/grafana/grafana-image-renderer/pull/619
    
    (cherry picked from commit ff2bc96567010b849e7307dfb767f7890f929180)

--- pkgs/by-name/gr/grafana-image-renderer/package.nix
+++ pkgs/by-name/gr/grafana-image-renderer/package.nix
@@ -2,10 +2,9 @@
   lib,
   fetchFromGitHub,
   fetchYarnDeps,
-  nodejs_22,
-  yarn2nix-moretea,
+  nodejs,
+  mkYarnPackage,
   runtimeShell,
-  yarn,
 }:
 
 # Notes for the upgrade:
@@ -13,72 +12,64 @@
 # * Replace new `package.json` here.
 # * Update `version`+`hash` and rebuild.
 
-let
-  yarn' = yarn.override { nodejs = nodejs_22; };
-in
-((yarn2nix-moretea.override {
-  yarn = yarn';
-  nodejs = nodejs_22;
-}).mkYarnPackage
-)
-  rec {
-    pname = "grafana-image-renderer";
-    version = "3.12.3";
+mkYarnPackage rec {
+  pname = "grafana-image-renderer";
+  version = "3.12.5";
 
-    src = fetchFromGitHub {
-      owner = "grafana";
-      repo = "grafana-image-renderer";
-      rev = "v${version}";
-      hash = "sha256-twB2V5iecLEMTrp+prFmDoJvcGLVQlLJ+DTSl/9V8S8=";
-    };
+  src = fetchFromGitHub {
+    owner = "grafana";
+    repo = "grafana-image-renderer";
+    rev = "v${version}";
+    hash = "sha256-dcWmMcvWwG4wGEEyFKa1R0jGGpK5x1F5Amr74JzJaLE=";
+  };
 
-    offlineCache = fetchYarnDeps {
-      yarnLock = src + "/yarn.lock";
-      hash = "sha256-TcsWApSR48OUIcQGko3XIaFCp22vFJbHcxrdFxUyRZU=";
-    };
+  offlineCache = fetchYarnDeps {
+    yarnLock = src + "/yarn.lock";
+    hash = "sha256-BBu+vOO0UgX3L7Svj0HgVKHR2lSe4tD6c9HDgJZdhHU=";
+  };
 
-    packageJSON = ./package.json;
+  packageJSON = ./package.json;
 
-    buildPhase = ''
-      runHook preBuild
+  buildPhase = ''
+    runHook preBuild
 
-      pushd deps/renderer
-      yarn run build
-      popd
+    pushd deps/renderer
+    yarn run build
+    popd
 
-      runHook postBuild
-    '';
+    runHook postBuild
+  '';
 
-    dontInstall = true;
+  dontInstall = true;
 
-    distPhase = ''
-      runHook preDist
+  distPhase = ''
+    runHook preDist
 
-      shopt -s extglob
+    shopt -s extglob
 
-      pushd deps/renderer
-      install_path="$out/libexec/grafana-image-renderer"
-      mkdir -p $install_path
-      cp -R ../../node_modules $install_path
-      cp -R ./!(node_modules) $install_path
-      popd
+    pushd deps/renderer
+    install_path="$out/libexec/grafana-image-renderer"
+    mkdir -p $install_path
+    cp -R ../../node_modules $install_path
+    cp -R ./!(node_modules) $install_path
+    popd
 
-      mkdir -p $out/bin
-      cat >$out/bin/grafana-image-renderer <<EOF
-      #! ${runtimeShell}
-      ${nodejs_22}/bin/node $install_path/build/app.js \$@
-      EOF
-      chmod +x $out/bin/grafana-image-renderer
+    mkdir -p $out/bin
+    cat >$out/bin/grafana-image-renderer <<EOF
+    #! ${runtimeShell}
+    ${nodejs}/bin/node $install_path/build/app.js \$@
+    EOF
+    chmod +x $out/bin/grafana-image-renderer
 
-      runHook postDist
-    '';
+    runHook postDist
+  '';
 
-    meta = with lib; {
-      homepage = "https://github.com/grafana/grafana-image-renderer";
-      description = "Grafana backend plugin that handles rendering of panels & dashboards to PNGs using headless browser (Chromium/Chrome)";
-      mainProgram = "grafana-image-renderer";
-      license = licenses.asl20;
-      maintainers = with maintainers; [ ma27 ];
-      platforms = platforms.all;
-    };
-  }
+  meta = with lib; {
+    homepage = "https://github.com/grafana/grafana-image-renderer";
+    description = "Grafana backend plugin that handles rendering of panels & dashboards to PNGs using headless browser (Chromium/Chrome)";
+    mainProgram = "grafana-image-renderer";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ ma27 ];
+    platforms = platforms.all;
+  };
+}
