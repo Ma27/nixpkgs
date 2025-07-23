40df1d89a4db31806d43342199c6ab779cda5cf6
Maximilian Bosch <maximilian@mbosch.me>
2025-07-23 15:37:49 +0200

    grafana-image-renderer: 3.12.7 -> 4.0.4
    ChangeLog: https://github.com/grafana/grafana-image-renderer/blob/v4.0.4/CHANGELOG.md
    (cherry picked from commit 005f4fd104cb908fe70e74f11a987b2a1b56ac23)

--- pkgs/by-name/gr/grafana-image-renderer/package.nix
+++ pkgs/by-name/gr/grafana-image-renderer/package.nix
@@ -14,18 +14,18 @@
 
 mkYarnPackage rec {
   pname = "grafana-image-renderer";
-  version = "3.12.7";
+  version = "4.0.4";
 
   src = fetchFromGitHub {
     owner = "grafana";
     repo = "grafana-image-renderer";
     rev = "v${version}";
-    hash = "sha256-X7aBUvyktqRHUGvIBBoqAHCUHbddU0ltyd3AhRW+PaA=";
+    hash = "sha256-+t2ZyMLqcjJaFnMuU+H6VxhWnwOibZCOhEmg7VCmQIs=";
   };
 
   offlineCache = fetchYarnDeps {
     yarnLock = src + "/yarn.lock";
-    hash = "sha256-lV+4r+5E55J4H1zl05SimxIhGVD/PvEkIr3j1yhZS4o=";
+    hash = "sha256-xZrIoQlPeyGTbFRUQ0M8Tc6YpzsC5IACW0bbZ+HnsOQ=";
   };
 
   packageJSON = ./package.json;
