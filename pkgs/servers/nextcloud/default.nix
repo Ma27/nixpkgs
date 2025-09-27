d0ef1d3b7aa753cb1a20022ed31e04f12595dc6a
provokateurin <kate@provokateurin.de>
2025-09-27 14:18:32 +0200

    nextcloud32: init at 32.0.0
    (cherry picked from commit d0192fd88e32062885bd109a581dd36af5f09ef0)

--- pkgs/servers/nextcloud/default.nix
+++ pkgs/servers/nextcloud/default.nix
@@ -7,6 +7,7 @@
   caBundle ? "${cacert}/etc/ssl/certs/ca-bundle.crt",
   nextcloud30Packages,
   nextcloud31Packages,
+  nextcloud32Packages,
 }:
 
 let
@@ -68,6 +69,12 @@ in
     version = "31.0.9";
     hash = "sha256-qrhBTMY1gco6jfRy9F60ErK4Q6lms4cCdUIbrQ1nD2g=";
     packages = nextcloud31Packages;
+  };
+
+  nextcloud32 = generic {
+    version = "32.0.0";
+    hash = "sha256-V0SNVdm4cQLxfDY0cyA0ahslj2AXxjuVjHie/ULjRaM=";
+    packages = nextcloud32Packages;
   };
 
   # tip: get the sha with:
