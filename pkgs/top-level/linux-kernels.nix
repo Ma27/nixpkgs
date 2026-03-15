a2a37ccc92c9ea397b0ce06f581943c0dc6e0dfa
Maximilian Bosch <maximilian@mbosch.me>
2026-03-15 13:42:20 +0100

    WIP

--- pkgs/top-level/linux-kernels.nix
+++ pkgs/top-level/linux-kernels.nix
@@ -31,6 +31,8 @@ in
 {
   kernelPatches = callPackage ../os-specific/linux/kernel/patches.nix { };
 
+  kconfig-ng = callPackage ../os-specific/linux/kernel/kconfig-ng {};
+
   kernels = recurseIntoAttrs (
     lib.makeExtensible (
       self:
