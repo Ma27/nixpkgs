040d648963dfba159bb767d29d35a464dd713442
Maximilian Bosch <maximilian@mbosch.me>
2026-03-28 18:39:53 +0100

    lasuite-docs{,-frontend,-collaboration-server}: 4.8.1 -> 4.8.4
    Changes:
    * https://github.com/suitenumerique/docs/releases/tag/v4.8.2
    * https://github.com/suitenumerique/docs/releases/tag/v4.8.3
    * https://github.com/suitenumerique/docs/releases/tag/v4.8.4
    
    (cherry picked from commit ff93136694ca1cc2d9ce34d54ac6d9fa1a00ff34)

--- pkgs/by-name/la/lasuite-docs-frontend/package.nix
+++ pkgs/by-name/la/lasuite-docs-frontend/package.nix
@@ -2,6 +2,7 @@
   lib,
   fetchFromGitHub,
   stdenv,
+  fetchpatch,
   fetchYarnDeps,
   nodejs,
   fixup-yarn-lock,
@@ -12,20 +13,29 @@
 
 stdenv.mkDerivation rec {
   pname = "lasuite-docs-frontend";
-  version = "4.8.1";
+  version = "4.8.4";
 
   src = fetchFromGitHub {
     owner = "suitenumerique";
     repo = "docs";
     tag = "v${version}";
-    hash = "sha256-R8DO7hsWt8+aKnHFEoZ06f1f+r8dNmNoPZRVBfr9VCY=";
+    hash = "sha256-k90JxFxXL3vEGBMkgbQABUCK99utJ88E/v9Zcj/2oBo=";
   };
 
   sourceRoot = "${src.name}/src/frontend";
 
+  patches = [
+    # https://github.com/suitenumerique/docs/pull/2147
+    (fetchpatch {
+      url = "https://github.com/Ma27/lasuite-docs/commit/f3b4ae26394b944cf5e666f983c3533cf32e6def.patch";
+      hash = "sha256-Ucw1KtsFrPvtoeeG2fH5L64Jfcog4RV38Qg+EykGcQY=";
+      stripLen = 2;
+    })
+  ];
+
   offlineCache = fetchYarnDeps {
     yarnLock = "${src}/src/frontend/yarn.lock";
-    hash = "sha256-F8VXjGY6Ct2Y8btqOmxZevCkxBvqg6xWZLYTZA2uUnM=";
+    hash = "sha256-ElI6WWKPCsO7Viexgp2XtcjXAXzFnG2ZPN5PjOaKO2g=";
   };
 
   nativeBuildInputs = [
